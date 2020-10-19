ed6f0e69f5195bf4a5fbe354cbcd0e49017547b9
Gidoras <iam@cijay.pw>
2020-10-19 12:19:02 +0300

    add High-level solution for reverse array string

--- reverseArrayString.js
+++ reverseArrayString.js
@@ -18,3 +18,15 @@ function spinWords(str){
     str = arraySTR.join(" ");
     return str;
   }
+
+/*
+  High-level solution:
+
+
+  function spinWords(words){
+    return words.split(' ').map(function (word) {
+      return (word.length > 4) ? word.split('').reverse().join('') : word;
+    }).join(' ');
+  }
+
+  */
